eac79dca855360610f6eebe17d244031b9980c79
RishiMaskare <maskarerishi54@gmail.com>
2026-01-07 14:38:01 +0530

    app.jsx issue fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import WriteLetter from "./pages/WriteLetter";
+import writeLetter from "./pages/WriteLetter";
 
 function App() {
   return (
